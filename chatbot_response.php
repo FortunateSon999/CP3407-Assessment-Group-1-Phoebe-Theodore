5442284996b093f0d0a83fba5b3b09150af2a5fe
Theodore Lee <qitheodore.lee@my.jcu.edu.au>
2024-07-28 21:50:49 +0800

    Minor changes to the chatbot.css and chatbot_response.php files.

--- chatbot_response.php
+++ chatbot_response.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 $request = json_decode(file_get_contents('php://input'), true);
 $message = $request['message'];
 
-$response = 'Hello, here are some things you can ask me about: \n 1) How do I book a car \n 2) Who can I contact';
+$response = "Hello, here are some things you can ask me about: \n 1) How do I book a car \n 2) Who can I contact";
 
 switch (strtolower($message)) {
     case 'hello':
